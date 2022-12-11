e10d75943dfa724505bb84cffb06dc58ed0221fe
ltammie <mactep70@gmail.com>
2022-12-11 18:47:15 +0300

    35. Search Insert Position

--- src/main/kotlin/com/ltammie/problems/solutions/arrays/ArrayList.java
+++ src/main/kotlin/com/ltammie/problems/solutions/arrays/ArrayList.java
@@ -69,4 +69,26 @@ public class ArrayList {
         }
         return slow + 1;
     }
+
+    /**
+     * 35
+     * binary search
+     */
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
 }
