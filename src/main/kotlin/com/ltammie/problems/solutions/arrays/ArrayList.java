865b19afc9ea34318e032238b5d88fde4f7094a3
ltammie <55656332+ltammie@users.noreply.github.com>
2022-12-11 16:59:26 +0300

    66. Plus One (#11)
    Co-authored-by: ltammie <mactep70@gmail.com>

--- src/main/kotlin/com/ltammie/problems/solutions/arrays/ArrayList.java
+++ src/main/kotlin/com/ltammie/problems/solutions/arrays/ArrayList.java
@@ -19,4 +19,20 @@ public class ArrayList {
         }
         return slow;
     }
+
+    /**
+     * @No 66
+     */
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
 }
