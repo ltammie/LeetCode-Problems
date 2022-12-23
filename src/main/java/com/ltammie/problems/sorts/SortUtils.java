1a888999e8c1e2d95ef0e82b9f1034a77f5c3445
ltammie <55656332+ltammie@users.noreply.github.com>
2022-12-23 03:30:15 +0300

    - quicksort (#16)
    - refactor
    - tests
    
    Co-authored-by: ltammie <mactep70@gmail.com>

--- src/main/java/com/ltammie/problems/sorts/SortUtils.java
+++ src/main/java/com/ltammie/problems/sorts/SortUtils.java
@@ -2,11 +2,11 @@ package com.ltammie.problems.sorts;
 
 public class SortUtils {
 
-    public static int[] bubbleSort(int[] array) {
+    public static void bubbleSort(int[] array) {
         if (array == null)
-            return null;
+            return;
         if (array.length <= 1)
-            return array;
+            return;
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
@@ -17,7 +17,32 @@ public class SortUtils {
                 }
             }
         }
+    }
 
-        return array;
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int part = partition(array, low, high);
+            quickSort(array, low, part - 1);
+            quickSort(array, part + 1, high);
+        }
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j <= high - 1; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
     }
 }
--- src/test/java/com/ltammie/problems/sorts/SortUtilsTest.java
+++ src/test/java/com/ltammie/problems/sorts/SortUtilsTest.java
@@ -3,14 +3,36 @@ package com.ltammie.problems.sorts;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class SortUtilsTest {
 
     @Test
     void bubbleSort() {
         int[] expected = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] actual = SortUtils.bubbleSort(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0});
+        int[] actual = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int[] nullArray = null;
+        int[] oneSizedArray = new int[]{999};
+
+        SortUtils.bubbleSort(actual);
+        SortUtils.bubbleSort(nullArray);
+        SortUtils.bubbleSort(oneSizedArray);
 
         assertArrayEquals(expected, actual);
+        assertNull(nullArray);
+        assertArrayEquals(new int[]{999}, oneSizedArray);
+    }
+
+    @Test
+    void quickSort() {
+        int[] expected = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] actual = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int[] oneSizedArray = new int[]{999};
+
+        SortUtils.quickSort(actual, 0, actual.length - 1);
+        SortUtils.quickSort(oneSizedArray, 0, oneSizedArray.length - 1);
+
+        assertArrayEquals(expected, actual);
+        assertArrayEquals(new int[]{999}, oneSizedArray);
     }
 }
