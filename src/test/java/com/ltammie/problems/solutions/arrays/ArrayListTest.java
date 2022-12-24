bcbaccea644bc1f257be4164e56a5753ba1163b4
ltammie <55656332+ltammie@users.noreply.github.com>
2022-12-25 01:35:43 +0300

    169. Majority Element (#18)
    Co-authored-by: ltammie <mactep70@gmail.com>

---
+++ src/test/java/com/ltammie/problems/solutions/arrays/ArrayListTest.java
@@ -0,0 +1,34 @@
+package com.ltammie.problems.solutions.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ArrayListTest {
+
+    @Test
+    void majorityElement() {
+        ArrayList array = new ArrayList();
+        int[] nums1 = new int[]{3, 2, 3};
+        int[] nums2 = new int[]{2, 2, 1, 1, 1, 2, 2};
+        int[] nums3 = new int[]{1};
+
+        assertEquals(3, array.majorityElement(nums1));
+        assertEquals(2, array.majorityElement(nums2));
+        assertEquals(1, array.majorityElement(nums3));
+    }
+
+    @Test
+    void majorityElementSort() {
+        ArrayList array = new ArrayList();
+        int[] nums1 = new int[]{3, 2, 3};
+        int[] nums2 = new int[]{2, 2, 1, 1, 1, 2, 2};
+        int[] nums3 = new int[]{1};
+        int[] nums4 = new int[]{10,2,5,4,3,7,9,0,1,8,6};
+
+        assertEquals(3, array.majorityElementSort(nums1));
+        assertEquals(2, array.majorityElementSort(nums2));
+        assertEquals(1, array.majorityElementSort(nums3));
+        assertEquals(1, array.majorityElementSort(nums4));
+    }
+}
