902a07859c658e246ac475c68a28ff5c5ada5a46
ltammie <mactep70@gmail.com>
2022-12-24 21:55:37 +0300

    - mergesort - randomized quicksort - test

--- src/main/java/com/ltammie/problems/sorts/SortUtils.java
+++ src/main/java/com/ltammie/problems/sorts/SortUtils.java
@@ -1,5 +1,7 @@
 package com.ltammie.problems.sorts;
 
+import java.util.Random;
+
 public class SortUtils {
 
     public static void bubbleSort(int[] array) {
@@ -19,15 +21,79 @@ public class SortUtils {
         }
     }
 
-    public static void quickSort(int[] array, int low, int high) {
+    /**
+     *
+     * @param array array to be sorted
+     * @param low lower bound of the array (or sub-array)
+     * @param high higher boun of the array (or sub-array)
+     * @param isRandom how a pivot point shoud be chosen - randomly or pick an element at array[high]
+     */
+    public static void quickSort(int[] array, int low, int high, boolean isRandom) {
         if (low < high) {
-            int part = partition(array, low, high);
-            quickSort(array, low, part - 1);
-            quickSort(array, part + 1, high);
+            int part = partition(array, low, high, isRandom);
+            quickSort(array, low, part - 1, isRandom);
+            quickSort(array, part + 1, high, isRandom);
         }
     }
 
-    private static int partition(int[] array, int low, int high) {
+    public static void mergeSort(int[] array, int low, int high) {
+        if (array == null)
+            return;
+        if (low < high) {
+
+            int mid = low + (high - low) / 2;
+            mergeSort(array, low, mid);
+            mergeSort(array, mid + 1, high);
+            merge(array, low, high, mid);
+        }
+    }
+
+    private static void merge(int[] array, int low, int high, int mid) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            left[i] = array[i + low];
+        for (int i = 0; i < n2; i++)
+            right[i] = array[i + mid + 1];
+
+        int i = 0;
+        int j = 0;
+        int k = low;
+        while (i < n1 && j < n2) {
+            if (left[i] < right[j]) {
+                array[k] = left[i];
+                i++;
+            } else {
+                array[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            array[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            array[k] = left[j];
+            j++;
+            k++;
+        }
+    }
+
+    private static int partition(int[] array, int low, int high, boolean isRandom) {
+        Random randomizer = new Random();
+        if (isRandom) {
+            int pivotIndex = randomizer.ints(low, high + 1)
+                    .findFirst()
+                    .getAsInt();
+            swap(array,pivotIndex,high);
+        }
+
         int pivot = array[high];
         int i = low - 1;
         for (int j = low; j <= high - 1; j++) {
--- src/test/java/com/ltammie/problems/sorts/SortUtilsTest.java
+++ src/test/java/com/ltammie/problems/sorts/SortUtilsTest.java
@@ -29,10 +29,32 @@ class SortUtilsTest {
         int[] actual = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
         int[] oneSizedArray = new int[]{999};
 
-        SortUtils.quickSort(actual, 0, actual.length - 1);
-        SortUtils.quickSort(oneSizedArray, 0, oneSizedArray.length - 1);
+        SortUtils.quickSort(actual, 0, actual.length - 1, false);
+        SortUtils.quickSort(oneSizedArray, 0, oneSizedArray.length - 1, false);
 
         assertArrayEquals(expected, actual);
         assertArrayEquals(new int[]{999}, oneSizedArray);
+    }
+    @Test
+    void quickSortRandom() {
+        int[] expected = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] actual = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int[] oneSizedArray = new int[]{999};
+
+        SortUtils.quickSort(actual, 0, actual.length - 1, true);
+        SortUtils.quickSort(oneSizedArray, 0, oneSizedArray.length - 1, true);
+
+        assertArrayEquals(expected, actual);
+        assertArrayEquals(new int[]{999}, oneSizedArray);
+    }
+
+    @Test
+    void mergeSort() {
+        int[] expected = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] actual = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+        SortUtils.mergeSort(actual, 0, actual.length - 1);
+
+        assertArrayEquals(expected, actual);
     }
 }
