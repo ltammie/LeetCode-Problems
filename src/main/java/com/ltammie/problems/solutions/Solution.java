907ebdbf0e2b77dee0ad33df14095855448e2507
ltammie <55656332+ltammie@users.noreply.github.com>
2023-03-31 21:38:05 +0300

    62 509

--- src/main/java/com/ltammie/problems/solutions/Solution.java
+++ src/main/java/com/ltammie/problems/solutions/Solution.java
@@ -41,5 +41,43 @@ public class Solution {
         return sum;
     }
 
+    /**
+     * @No 62 Unique Paths
+     * @algo dynamic programming
+     */
+    public int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0)
+            return 0;
+        int[][] grid = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            grid[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            grid[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+
+    /**
+     * @No 542 01 Matrix
+     * @alog dfs
+     */
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1)
+                    continue;
+            }
+        }
+        return mat;
+    }
+
 
 }
