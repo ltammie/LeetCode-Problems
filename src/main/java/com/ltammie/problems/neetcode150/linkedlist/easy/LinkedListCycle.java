cc2229c1cec77b6fe8c3fbc6152ff5c2496daad1
ltammie <55656332+ltammie@users.noreply.github.com>
2023-11-13 00:45:54 +0300

    nc150 (#38)

---
+++ src/main/java/com/ltammie/problems/neetcode150/linkedlist/easy/LinkedListCycle.java
@@ -0,0 +1,23 @@
+package com.ltammie.problems.neetcode150.linkedlist.easy;
+
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
+
+        }
+
+        return false;
+    }
+
+}
